147833fc64a9a4243670fc4e363dd0b11a6b4913
jbuguy <wassimnefzi120@gmail.com>
2025-05-18 12:11:37 +0100

    master and worker disturbee added the functionaltiy to conniucate task still need to add error handeling

---
+++ distribuee/master/master.go
@@ -0,0 +1,74 @@
+package master
+
+import (
+	"net"
+	"net/rpc"
+	"sync"
+)
+
+type Task struct {
+	jobName    string
+	taskNumber int
+	inFile     string
+	funcName   string
+	nReduce    int
+}
+type Master struct {
+	tasks   []Task
+	waiting []chan Task
+	mu      sync.Mutex
+}
+
+type KeyValue struct {
+	Key   string
+	Value string
+}
+type Args struct{}
+type Args2 struct {
+	jobName    string
+	taskNumber int
+}
+
+func (master *Master) getTask(args Args, reply *Task) error {
+	taskchan := make(chan Task, 1)
+	master.mu.Lock()
+	if len(master.tasks) > 0 {
+		*reply = master.tasks[0]
+		master.tasks = master.tasks[1:]
+		return nil
+	}
+	master.waiting = append(master.waiting, taskchan)
+	master.mu.Unlock()
+	*reply = <-taskchan
+	return nil
+
+}
+func (master *Master) addTask(task Task) {
+	master.mu.Lock()
+	defer master.mu.Unlock()
+	if len(master.waiting) > 0 {
+		workerChan := master.waiting[0]
+		master.waiting = master.waiting[1:]
+		workerChan <- task
+	} else {
+		master.tasks = append(master.tasks, task)
+	}
+}
+func (Master *Master) ReportTaskDone(Args2, reply bool) {
+
+}
+func main() {
+	master := new(Master)
+	rpc.Register(master)
+	listener, err := net.Listen("tcp", ":1234")
+	if err != nil {
+		return
+	}
+	for {
+		conn ,err:=listener.Accept()
+		if err !=nil{
+			continue
+		}
+		go rpc.ServeConn(conn)
+	}
+}
---
+++ distribuee/worker/worker.go
@@ -0,0 +1,69 @@
+package worker
+
+import (
+	"log"
+	"math/rand"
+	"net/rpc"
+	"time"
+)
+
+type Worker struct {
+}
+type Args2 struct {
+	jobName    string
+	taskNumber int
+}
+type KeyValue struct {
+	Key   string
+	Value string
+}
+type Args struct{}
+type Reply1 struct {
+	jobName    string
+	taskNumber int
+	inFile     string
+	funcName   string
+	nReduce    int
+}
+
+func (simulate Worker) simulate(client *rpc.Client, p1, p2 float64) {
+	for {
+		var reply Reply1
+		client.Call("master.getTask", Args{}, &reply)
+		random := rand.Float64()
+		if random < p1 {
+			time.Sleep(15)
+		}
+		random = rand.Float64()
+		if random < p2 {
+			return
+		}
+		switch reply.jobName {
+		case "map":
+			doMap(reply.jobName, reply.taskNumber, reply.inFile, reply.nReduce, mapF)
+			break
+		case "reduce":
+			doReduce(reply.jobName, reply.taskNumber, reply.inFile, reduceF)
+			break
+		}
+		var tmp bool
+		client.Call("master.ReportTaskDone", Args2{reply.jobName, reply.taskNumber}, tmp)
+		if tmp {
+			return
+		}
+	}
+}
+func main() {
+	client, err := rpc.Dial("tcp", "localhost:1234")
+	defer client.Close()
+	if err != nil {
+		log.Fatal("Dialing:", err)
+	}
+	worker := new(Worker)
+	go worker.simulate(client, 0.1, 0.01)
+}
+func doMap(jobName string, mapTaskNumber int, inFile string, nReduce int, mapF func(
+	file string, contents string) []KeyValue)
+func mapF(document string, content string) []KeyValue
+func reduceF(key string, values []string) string
+func doReduce(jobName string, reduceTaskNumber int, inFile string, reduceF func(key string, values []string) string)
