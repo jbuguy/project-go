d9e1e201835a33039d39044f3ae753072be21416
jbuguy <wassimnefzi120@gmail.com>
2025-05-18 10:12:20 +0100

    changed KeyValue to be a concerte type

--- worker.go
+++ worker.go
@@ -6,30 +6,38 @@ import (
 	"strings"
 )
 
-type KeyValue[Key, Value interface{}] struct {
-	key   Key
-	value Value
+type KeyValue struct {
+	key   string
+	value string
+}
+type IWorker interface {
+	doMap(jobName string, mapTaskNumber int, inFile string, nReduce int, mapF func(
+		file string, contents string) []KeyValue)
 }
 
-func mapF(document, content string) []KeyValue[string, int] {
+func mapF(document, content string) []KeyValue {
 	counts := make(map[string]int)
 	words := strings.Fields(content)
 	for _, word := range words {
 		counts[strings.ToLower(word)] += 1
 	}
-	var result []KeyValue[string, int]
+	var result []KeyValue
 	for k, v := range counts {
-		result = append(result, KeyValue[string, int]{k, v})
+		result = append(result, KeyValue{k, fmt.Sprint(v)})
 	}
 	return result
 }
 func reduceF(key string, values []string) string {
 	count := 0
 	for _, v := range values {
-		value,err:= strconv.Atoi(v)
-		if err==nil{
-			count+=value
+		value, err := strconv.Atoi(v)
+		if err == nil {
+			count += value
 		}
 	}
 	return fmt.Sprint(count)
+}
+func doMap(jobName string, mapTaskNumber int, inFile string, nReduce int, mapF func(
+	file string, contents string) []KeyValue) {
+
 }
