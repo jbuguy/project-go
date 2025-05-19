d5cff1dbc3a86150d0687295dbb36a0a55a52d6f
jbuguy <wassimnefzi120@gmail.com>
2025-05-19 09:58:08 +0100

    added map reduce to seq

--- seq/mapreduce/mapreduce.go
+++ seq/mapreduce/mapreduce.go
@@ -1,6 +1,7 @@
 package mapreduce
 
 import (
+	"bufio"
 	"fmt"
 	"hash/fnv"
 	"os"
@@ -53,69 +54,6 @@ func ihash(s string) uint32 {
 
 // doMap applique la fonction mapF, et sauvegarde les résultats.
 // A COMPLETER
-func DoMap(
-	jobName string,
-	mapTaskNumber int,
-	inFile string,
-	nReduce int,
-	mapF func(string, string) []KeyValue,
-) {
-	data, _ := os.ReadFile(inFile)
-	content := string(data)
-	kvs := mapF(fmt.Sprintf("file.part%d", mapTaskNumber), string(content))
-	sort.slice(kvs ,func(i, j int) bool {
-		return kvs[i].key < kvs[j].key
-	})
-	files := make([]*os.File, nReduce)
-	
-	for i := range nReduce {
-		name := ReduceName(jobName, mapTaskNumber, i)
-		file, _ := os.Create(name)
-		files = append(files, file)
-		defer file.Close()
-	}
-	for _, v := range kvs {
-		index := ihash(v.Key) % uint32(nReduce)
-		name := ReduceName(jobName, mapTaskNumber, int(index))
-		file, err := os.OpenFile(name, os.O_APPEND, 0644)
-		if err != nil {
-			continue
-		}
-		file.WriteString(fmt.Sprintf("%s\n", v.Value))
-	}
-}
-
-// doReduce effectue une tâche de réduction en lisant les fichiers
-// intermédiaires, en regroupant les valeurs par clé, et en appliquant
-// la fonction reduceF.
-// A COMPLETER
-func DoReduce(
-	jobName string,
-	reduceTaskNumber int,
-	nMap int,
-	reduceF func(key string, values []string) string,
-) {
-	// Créer une map pour stocker les valeurs par clé
-
-	// Lire les fichiers intermédiaires produits par chaque tâche map
-	for i := 0; i < nMap; i++ {
-
-		// Ouvrir le fichier pour la tâche de mappage i
-
-		// Lire les paires clé-valeur du fichier
-		// Ajouter la valeur à la liste de valeurs pour cette clé
-
-	}
-	// Ouvrir le fichier de sortie pour la tâche de réduction
-	// utiliser mergeName
-
-	// Appliquer la fonction de réduction à chaque clé
-	for _, key := range keys {
-		// Appliquer reduceF pour réduire les valeurs associées à cette clé
-		// Écrire la clé et la valeur réduite dans le fichier de sortie
-	}
-}
-
 func mapF(document string, content string) []KeyValue {
 	words := strings.Fields(content)
 	kvs := []KeyValue{}
@@ -129,7 +67,38 @@ func mapF(document string, content string) []KeyValue {
 
 	return kvs
 }
-
 func reduceF(key string, values []string) string {
 	return fmt.Sprintf("%d", len(values))
+}
+func DoReduce(
+	jobName string,
+	reduceTaskNumber int,
+	nMap int,
+	reduceF func(key string, values []string) string,
+) {
+	// Créer une map pour stocker les valeurs par clé
+	m := make(map[string][]string)
+	// Lire les fichiers intermédiaires produits par chaque tâche map
+	for i := 0; i < nMap; i++ {
+		// Ouvrir le fichier pour la tâche de mappage i
+		file, _ := os.Open(ReduceName(jobName, i, reduceTaskNumber))
+		// Lire les paires clé-valeur du fichier
+		sc := bufio.NewScanner(file)
+		for sc.Scan() {
+			// Ajouter la valeur à la liste de valeurs pour cette clé
+			line := strings.Split(sc.Text(), " ")
+			m[line[0]] = append(m[line[0]], line[1])
+		}
+	}
+	// Ouvrir le fichier de sortie pour la tâche de réduction
+	// utiliser mergeName
+	outName := MergeName(jobName, reduceTaskNumber)
+	file, _ := os.OpenFile(outName, os.O_APPEND|os.O_CREATE, 0644)
+	// Appliquer la fonction de réduction à chaque clé
+	for key, values := range m {
+		// Appliquer reduceF pour réduire les valeurs associées à cette clé
+		// Écrire la clé et la valeur réduite dans le fichier de sortie
+		res := reduceF(key, values)
+		file.WriteString(res + "\n")
+	}
 }
