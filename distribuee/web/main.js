7ac3e7f380709bb316fb0e6c8b2f3be91e9272b2
jbuguys <wassimnefzi120@gmail.com>
2025-05-19 15:03:57 +0100

    aded progress bar logic

--- distribuee/web/main.js
+++ distribuee/web/main.js
@@ -1,11 +1,12 @@
+i=1
 async function fetch_status() {
     const res = await fetch("/status");
     const d = await res.json();
     document.getElementById("status").innerHTML = d.stats.map(s =>
     `<p>${s.task_id}: ${s.status}</p>`).join("");
+    move(d.progress);
 }
-setInterval(fetch_status, 2000);
-fetch_status()
+
 function start() {
     const nMap = document.getElementById("nMap").value;
     const nReduce = document.getElementById("nReduce").value;
@@ -15,4 +16,20 @@ function start() {
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ nMap: Number(nMap), nReduce: Number(nReduce) })
     })
+    setInterval(fetch_status, 20);
+    fetch_status();
+}
+function move(pr) {
+    if (i == 1) {
+        var elem = document.getElementById("myBar");
+        var id = setInterval(frame, 10);
+        function frame() {
+            if (pr >= 100) {
+                clearInterval(id);
+                i = 0;
+            } else {
+                elem.style.width =pr.tofixed(2) + "%";
+            }
+        }
+    }
 }
